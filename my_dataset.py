f4a76f2d58ba6f2e4624819f43586ea44fe1ef4b
WenhanGuo <wgaa2021@mymail.pomona.edu>
2023-06-12 15:42:05 +0800

    split ImageDataset class into its own script

---
+++ my_dataset.py
@@ -0,0 +1,47 @@
+from torch.utils.data import Dataset
+import os
+import numpy as np
+import pandas as pd
+import astropy.io.fits as fits
+
+
+# Dataset class for our own data structure
+class ImageDataset(Dataset):
+    def __init__(self, catalog, data_dir, transform=None, target_transform=None):
+        """
+        catalog: name of .csv file containing image names to be read
+        data_dir: path to data directory containing gamma1, gamma2, kappa folders
+        transform: transformations to input data (gamma) prior to training
+        target_transform: transformation to target data (kappa) prior to training 
+        """
+        self.img_names = pd.read_csv(catalog)
+        self.data_dir = data_dir
+        self.transform = transform
+        self.target_transform = target_transform
+    
+    def __len__(self):
+        return len(self.img_names)
+    
+    def read_img(self, idx, img_type=['gamma1', 'gamma2', 'kappa']):
+        img_name = self.img_names[img_type][idx]   # get single image name
+        img_path = os.path.join(self.data_dir, img_type, img_name)
+        with fits.open(img_path, memmap=False) as f:
+            img = f[0].data   # read image into numpy array
+        return np.float32(img)   # force apply float32 to resolve endian conflict
+    
+    def __getitem__(self, idx):
+        # read in images
+        gamma1 = self.read_img(idx, img_type='gamma1')
+        gamma2 = self.read_img(idx, img_type='gamma2')
+        kappa = self.read_img(idx, img_type='kappa')
+        # reformat data shapes
+        gamma = np.array([gamma1, gamma2])
+        gamma = np.moveaxis(gamma, 0, 2)
+        kappa = np.expand_dims(kappa, 2)
+        # apply transforms
+        if self.transform:
+            gamma = self.transform(gamma)
+        if self.target_transform:
+            kappa = self.target_transform(kappa)
+        # gamma shape: torch.Size([2, 512, 512]); kappa shape: torch.Size([1, 512, 512])
+        return gamma, kappa
