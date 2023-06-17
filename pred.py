2e177b124571988e143ef57707b59b3cb3f90375
WenhanGuo <wgaa2021@mymail.pomona.edu>
2023-06-17 22:21:54 +0800

    resturcture argparse

--- pred.py
+++ pred.py
@@ -9,11 +9,6 @@ import pandas as pd
 import astropy.io.fits as fits
 
 
-# define training device (cpu/gpu)
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print('device =', device)
-
-
 def save_img(pred, true, res, fname):
     hdu = fits.PrimaryHDU(pred)
     hdu.writeto(fname, overwrite=True)
@@ -21,12 +16,6 @@ def save_img(pred, true, res, fname):
     fits.append(fname, res)
     return
 
-def get_args():
-    parser = argparse.ArgumentParser(description='Predict kappa from test shear')
-    parser.add_argument('name', type=str, help='name of weights file')
-    parser.add_argument("--num", default=32, type=int, help='number of test images to run')
-    parser.add_argument("--dir", default='/share/lirui/Wenhan/WL/data_new', type=str, help='data directory')
-    return parser.parse_args()
 
 def main(args):
     test_cat = pd.read_csv(os.path.join(args.dir, 'test.csv'))
@@ -38,7 +27,7 @@ def main(args):
                      transforms=T.Compose([
                          T.ToTensor()
                          ]), 
-                     gaus_noise=T.AddGaussianNoise(n_galaxy=50)
+                     gaus_noise=T.AddGaussianNoise(n_galaxy=args.n_galaxy)
                      )
     test_data = ImageDataset(catalog=os.path.join(args.dir, 'test.csv'), **test_args)
     test_data = Subset(test_data, np.arange(args.num))
@@ -65,6 +54,20 @@ def main(args):
             test_step += 1
 
 
+def get_args():
+    parser = argparse.ArgumentParser(description='Predict kappa from test shear')
+    parser.add_argument('name', type=str, help='name of weights file')
+    parser.add_argument("-g", "--n-galaxy", default=50, type=float, help='number of galaxies per arcmin (to determine noise level)')
+    parser.add_argument("--num", default=32, type=int, help='number of test images to run')
+    parser.add_argument("--dir", default='/share/lirui/Wenhan/WL/data_new', type=str, help='data directory')
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
     args = get_args()
+
+    # define testing device (cpu/gpu)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print('device =', device)
+
     main(args)
