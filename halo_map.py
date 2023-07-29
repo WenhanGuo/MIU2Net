886d585c27b79426d7e7ce59619b9ea0aaaf2df9
WenhanGuo <109452436+WenhanGuo@users.noreply.github.com>
2023-07-29 22:55:59 +0800

    variable referent fix

--- halo_map.py
+++ halo_map.py
@@ -7,17 +7,17 @@ import astropy.cosmology.units as cu
 import astropy.io.fits as fits
 
 
-def cat_to_map(halo_cat, map_type, out_dir):
+def cat_to_map(halo_cat, z_list, map_type, out_dir):
     m = HaloMap3D(halo_cat, z_list)
     m.map_all(map_type=map_type)
     hdu = fits.PrimaryHDU(m.data)
     hdulist = fits.HDUList([hdu])
     hdulist.writeto(os.path.join(out_dir, m.name+'_nfw.fits'), overwrite=True)
 
-def multiprocess_wrapper(args):
+def multiprocess_wrapper(args, cat_name):
     halo_cat = pd.read_csv(os.path.join(args.cat_dir, cat_name), sep='\t')
     halo_cat.name = cat_name[:-4]
-    cat_to_map(halo_cat, args.map_type, args.out_dir)
+    cat_to_map(halo_cat, args.z_list, args.map_type, args.out_dir)
 
 def get_args():
     import argparse
@@ -35,11 +35,11 @@ if __name__ == '__main__':
     args = get_args()
     halo_cats = sorted(glob1(args.cat_dir, '*area[0-9].txt'))[:args.num]
     redshift_cat = pd.read_csv(args.zcat_path, sep=' ')
-    z_list = list(redshift_cat['z_lens']) * cu.redshift
+    args.z_list = list(redshift_cat['z_lens']) * cu.redshift
 
     processes = []
     for cat_name in halo_cats:
-        p = Process(target=multiprocess_wrapper, args=(args,))
+        p = Process(target=multiprocess_wrapper, args=(args, cat_name))
         processes.append(p)
         p.start()
 
