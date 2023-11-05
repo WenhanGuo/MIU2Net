a5b29889f349eec0396addf5fb639e7ff46d8fa1
WenhanGuo <109452436+WenhanGuo@users.noreply.github.com>
2023-11-05 21:17:04 +0545

    add on-site mcalens reconstruction

--- make_master_cubes_multiproc.py
+++ make_master_cubes_multiproc.py
@@ -4,7 +4,12 @@ import numpy as np
 from astropy.io import fits
 from my_cosmostat.astro.wl.mass_mapping import massmap2d, shear_data
 import glob
-
+try:
+    import pysparse
+except ImportError:
+    print(
+        "Warning in make_master_cubes_multiproc.py: do not find pysap bindings ==> use slow python code. "
+    )
 
 def shape_noise(n_galaxy):
     sigma_e = 0.4   # rms amplitude of the intrinsic ellipticity distribution
@@ -15,13 +20,14 @@ def shape_noise(n_galaxy):
     return std
 
 
-def save_cube(true, ml, ks, wiener, sparse, save_dir, save_name):
+def save_cube(true, ml, ks, wiener, sparse, mcalens, save_dir, save_name):
     true1 = np.expand_dims(true, axis=0)
     ml1 = np.expand_dims(ml, axis=0)
     ks1 = np.expand_dims(ks, axis=0)
     wiener1 = np.expand_dims(wiener, axis=0)
     sparse1 = np.expand_dims(sparse, axis=0)
-    c = np.concatenate([true1, ml1, ks1, wiener1, sparse1])
+    mcalens1 = np.expand_dims(mcalens, axis=0)
+    c = np.concatenate([true1, ml1, ks1, wiener1, sparse1, mcalens1])
     fits.writeto(os.path.join(save_dir, save_name), data=np.float32(c), overwrite=True)
     print(f'saved cube {save_name}')
 
@@ -33,9 +39,15 @@ def make_cube(args, fname):
         shear2 = cube[1]
         ks = cube[2]
         wiener = cube[3]
-        pred = cube[4]
-        true = cube[5]
-        res = cube[6]
+        if args.have_mcalens == True:
+            mcalens = cube[4]
+            pred = cube[5]
+            true = cube[6]
+            res = cube[7]
+        else:
+            pred = cube[4]
+            true = cube[5]
+            res = cube[6]
 
     # initialize CosmoStat shear class
     D = shear_data()
@@ -48,12 +60,32 @@ def make_cube(args, fname):
 
     # create the mass mapping structure and initialize it
     M = massmap2d(name='mass')
-    M.init_massmap(nx=512, ny=512)
+    psWT_gen1 = pysparse.MRStarlet(bord=1, gen2=False, nb_procs=1, verbose=0)
+    psWT_gen2 = pysparse.MRStarlet(bord=1, gen2=True, nb_procs=1, verbose=0)
+    M.init_massmap(nx=512, ny=512, pass_class=[psWT_gen1, psWT_gen2])
 
     # sparse reconstruction with a 5 sigma detection
-    sparse, ti = M.sparse_recons(D, UseNoiseRea=False, Nsigma=5, ThresCoarse=False, Inpaint=False, Nrea=None)
+    sparse, ti = M.sparse_recons(InshearData=D, 
+                               UseNoiseRea=False, 
+                               niter=12, 
+                               Nsigma=5, 
+                               ThresCoarse=False, 
+                               Inpaint=False, 
+                               pass_class=[psWT_gen1, psWT_gen2])
 
-    save_cube(true, pred, ks, wiener, sparse, save_dir=args.save_dir, save_name=fname)
+    if args.have_mcalens == False:
+        # mcalens reconstruction
+        p_signal = fits.open('./signal_power_spectrum.fits')[0].data
+        mcalens, k1i5, k2r5, k2i = M.sparse_wiener_filtering(InshearData=D, 
+                                                        PowSpecSignal=p_signal,
+                                                        niter=12, 
+                                                        Nsigma=5, 
+                                                        Inpaint=False, 
+                                                        Bmode=False, 
+                                                        ktr=None, 
+                                                        pass_class=[psWT_gen1, psWT_gen2])
+
+    save_cube(true, pred, ks, wiener, sparse, mcalens, save_dir=args.save_dir, save_name=fname)
 
 
 def get_args():
@@ -63,6 +95,7 @@ def get_args():
     parser.add_argument("--save-dir", default='/share/lirui/Wenhan/WL/kappa_map/result/master_cubes', type=str)
     parser.add_argument("--cpu", default=32, type=int, help='number of cpu cores to use for multiprocessing')
     parser.add_argument("--n-galaxy", default=50, type=int)
+    parser.add_argument("--have-mcalens", default=False, action='store_true')
     return parser.parse_args()
 
 
@@ -71,4 +104,4 @@ if __name__ == '__main__':
     fnames = glob.glob1(args.dir, '*.fits')
     with multiprocessing.Pool(processes=args.cpu) as pool:
         arguments = [(args, fname) for fname in fnames]
-        pool.starmap(func=make_cube, iterable=arguments, chunksize=2)
+        pool.starmap(func=make_cube, iterable=arguments)
