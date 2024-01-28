2596c42352fb702810d12293e2ff784df515f876
WenhanGuo <109452436+WenhanGuo@users.noreply.github.com>
2024-01-27 17:43:44 -0800

    reduced shear, random mask, seed for shape noise

--- transforms.py
+++ transforms.py
@@ -6,6 +6,9 @@ import numpy as np
 from typing import Sequence
 from my_cosmostat.astro.wl.mass_mapping import massmap2d, shear_data
 from astropy.io import fits
+from astropy.modeling.models import Disk2D
+from astropy.table import QTable
+from photutils.datasets import make_model_sources_image
 try:
     import pysparse
 except ImportError:
@@ -94,6 +97,16 @@ class CenterCrop(object):
         return image, target
 
 
+class Resize(object):
+    def __init__(self, size):
+        self.size = size
+    
+    def __call__(self, image, target):
+        image = F.resize(image, size=self.size, antialias=True)
+        target = F.resize(target, size=self.size, antialias=True)
+        return image, target
+
+
 # add gaussian noise to shear
 class AddGaussianNoise(object):
     def __init__(self, args):
@@ -104,26 +117,62 @@ class AddGaussianNoise(object):
         """
         self.n_galaxy = args.n_galaxy
         sigma_e = 0.4   # rms amplitude of the intrinsic ellipticity distribution
-        theta_G = 0.205   # pixel side length in arcmin (gaussian smoothing window)
+        theta_G = 3.5*60/(1024/args.crop)/args.crop   # pixel side length in arcmin (gaussian smoothing window)
         variance = (sigma_e**2 / 2) / (theta_G**2 * self.n_galaxy)
         self.std = np.sqrt(variance)
         self.noise_seed = args.noise_seed
+        # for 50 galaxies per arcmin^2, std = 0.1951; 
+        # for 20 galaxies per arcmin^2, std = 0.3085
 
     def __call__(self, image, target):
         if self.noise_seed == 0:
             noise = torch.randn(image.shape) * self.std
         else:
             assert type(self.noise_seed) == int
-            seed = int(image[0][0][0] * 1e9)  # set random seed by the top left pixel of gamma1
-            mod_seed = [*range(1, self.noise_seed+1, 1)]  # construct list of [1, 2, 3, etc.] for seed modification
-            # round seed to nearest 10, then add a choice of modification with equal prob for each
+            # set random seed by the top left pixel of gamma1
+            seed = int(image[0][0][0] * 1e9)
+            # construct list of [1, 2, ..., n_realizations] for seed modification
+            mod_seed = [*range(1, self.noise_seed+1, 1)]
+            # round seed to nearest 10, then add a random choice of modification with equal prob for each
             seed = round(seed, ndigits=-1) + random.choice(mod_seed)
             g = torch.Generator()  # pseudo random generator
             noise = torch.randn(image.shape, generator=g.manual_seed(seed)) * self.std
-
         return image + noise, target
-        # for 50 galaxies per arcmin^2, std = 0.1951; 
-        # for 20 galaxies per arcmin^2, std = 0.3085
+
+
+# add bright star masks to shear
+class AddStarMask(object):
+    def __init__(self, args, fill_value=0):
+        self.size = args.resize
+        self.n_sources = int(1100 * args.mask_frac)
+        self.model = Disk2D()
+        self.rng = np.random.default_rng(seed=None)
+        self.fill_value = fill_value
+
+    def __call__(self, image, target):
+        sources = QTable()
+        sources['amplitude'] = np.ones(self.n_sources)
+        sources['x_0'] = self.rng.uniform(low=0, high=self.size, size=self.n_sources)
+        sources['y_0'] = self.rng.uniform(low=0, high=self.size, size=self.n_sources)
+        sources['R_0'] = self.rng.power(a=0.3, size=self.n_sources) * 13
+        data = make_model_sources_image(shape=(self.size, self.size), 
+                                        model=self.model, 
+                                        source_table=sources)
+        mask = torch.tensor((data < 1)).repeat(image.shape[0], 1, 1)
+        image[mask == 0] = self.fill_value
+        return image, target
+
+
+# use reduced shear (g) instead of shear (gamma)
+class ReducedShear(object):
+    def __init__(self, args):
+        self.mode = args.reduced_shear
+    
+    def __call__(self, image, target):
+        if self.mode == True:
+            image = image / (1 - target)  # gamma = gamma / (1 - kappa)
+            return image, target
+        return image, target
 
 
 class KS_rec(object):
@@ -136,7 +185,6 @@ class KS_rec(object):
         self.psWT_gen1 = pysparse.MRStarlet(bord=1, gen2=False, nb_procs=1, verbose=0)
         self.psWT_gen2 = pysparse.MRStarlet(bord=1, gen2=True, nb_procs=1, verbose=0)
         self.M.init_massmap(nx=1024, ny=1024, pass_class=[self.psWT_gen1, self.psWT_gen2])
-        print('KS initialized')
 
     def shear_rec(self, shear1, shear2):
         ks =  self.M.g2k(shear1, shear2, pass_class=[self.psWT_gen1, self.psWT_gen2])
@@ -149,7 +197,7 @@ class KS_rec(object):
         elif self.mode == 'add':
             # perdict kappa using KS and add it as a 3rd channel to gamma
             # if ks add: image shape = torch.Size([3, 512, 512]); last channel is ks map
-            ks_kappa = self.shear_rec(-image[0], image[1])   # negative sign is important
+            ks_kappa = self.shear_rec(image[0], image[1])   # negative sign is important
             ks_kappa = torch.FloatTensor(ks_kappa)
             image = torch.concat((image, ks_kappa.unsqueeze(0)), dim=0)
             return image, target
@@ -157,7 +205,7 @@ class KS_rec(object):
         elif self.mode == 'only':
             # perdict kappa using KS and remove shear information
             # if ks only: image shape = torch.Size([1, 512, 512])
-            ks_kappa = self.shear_rec(-image[0], image[1])   # negative sign is important
+            ks_kappa = self.shear_rec(image[0], image[1])   # negative sign is important
             ks_kappa = torch.FloatTensor(ks_kappa)
             image = ks_kappa.unsqueeze(0)
             return image, target
@@ -179,7 +227,6 @@ class Wiener(object):
         self.psWT_gen1 = pysparse.MRStarlet(bord=1, gen2=False, nb_procs=1, verbose=0)
         self.psWT_gen2 = pysparse.MRStarlet(bord=1, gen2=True, nb_procs=1, verbose=0)
         self.M.init_massmap(nx=512, ny=512, pass_class=[self.psWT_gen1, self.psWT_gen2])
-        print('wiener initialized')
 
     def wiener(self, shear1, shear2):
         retr, reti = self.M.wiener(shear1, shear2, 
@@ -193,13 +240,13 @@ class Wiener(object):
             return image, target
         
         elif self.mode == 'add':
-            wf_kappa, _ = self.wiener(-image[0], image[1])   # negative sign is important
+            wf_kappa, _ = self.wiener(image[0], image[1])   # negative sign is important
             wf_kappa = torch.FloatTensor(wf_kappa)
             image = torch.concat((image, wf_kappa.unsqueeze(0)), dim=0)
             return image, target
         
         elif self.mode == 'only':        
-            wf_kappa, _ = self.wiener(-image[0], image[1])   # negative sign is important
+            wf_kappa, _ = self.wiener(image[0], image[1])   # negative sign is important
             wf_kappa = torch.FloatTensor(wf_kappa)
             image = wf_kappa.unsqueeze(0)
             return image, target
@@ -226,8 +273,6 @@ class sparse(object):
         CovMat = np.ones((512, 512)) * (std**2)
         self.D.Ncov = CovMat
 
-        print('sparse initialized')
-
     def sparse(self, shear1, shear2):
         self.D.g1 = shear1
         self.D.g2 = shear2
@@ -246,7 +291,7 @@ class sparse(object):
             return image, target
         
         elif self.mode == 'add':
-            sp_kappa, _ = self.sparse(np.float32(-image[0]), np.float32(image[1]))   # negative sign is important
+            sp_kappa, _ = self.sparse(np.float32(image[0]), np.float32(image[1]))   # negative sign is important
             sp_kappa = np.float32(sp_kappa)
             image = np.concatenate([image, np.expand_dims(sp_kappa, axis=0)], axis=0)
             return image, target
@@ -274,8 +319,6 @@ class MCALens(object):
         CovMat = np.ones((512, 512)) * (std**2)
         self.D.Ncov = CovMat
 
-        print('MCALens initialized')
-
     def mcalens(self, shear1, shear2):
         self.D.g1 = shear1
         self.D.g2 = shear2
@@ -295,7 +338,7 @@ class MCALens(object):
             return image, target
         
         elif self.mode == 'add':
-            mca_kappa, _, _, _ = self.mcalens(np.float32(-image[0]), np.float32(image[1]))   # negative sign is important
+            mca_kappa, _, _, _ = self.mcalens(np.float32(image[0]), np.float32(image[1]))   # negative sign is important
             # mca_kappa = torch.FloatTensor(mca_kappa)
             mca_kappa = np.float32(mca_kappa)
             # image = torch.concat((image, mca_kappa.unsqueeze(0)), dim=0)
